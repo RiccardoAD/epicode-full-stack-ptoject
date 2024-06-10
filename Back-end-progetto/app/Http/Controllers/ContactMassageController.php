2ea80bf1df95c7276c3fe77688a27f1a3a5e2ad9
RiccardoAD <rick_95@libero.it>
2024-06-10 22:13:58 +0200

    0.01

---
+++ Back-end-progetto/app/Http/Controllers/ContactMassageController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Contact_massage;
+use App\Http\Requests\StoreContact_massageRequest;
+use App\Http\Requests\UpdateContact_massageRequest;
+
+class ContactMassageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreContact_massageRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Contact_massage $contact_massage)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Contact_massage $contact_massage)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateContact_massageRequest $request, Contact_massage $contact_massage)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Contact_massage $contact_massage)
+    {
+        //
+    }
+}
